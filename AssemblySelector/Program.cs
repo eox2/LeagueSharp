da55528cfbe919f205a7d4740b631a4247f76c21
SephLeague <lazernr@gmail.com>
2014-11-25 14:11:33 -0500

    Added assembly selector

---
+++ AssemblySelector/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+using SharpDX;
+using System.Runtime.InteropServices;
+using AssemblySelector.Properties;
+
+namespace AssemblySelector
+{
+    class Selector
+    {
+        private static readonly Vector2 _scale = new Vector2(1.25f, 1.25f);
+        private static Render.Sprite Leaguesharpicon;
+        private static Vector2 _posLsharp = new Vector2(Drawing.Width / 2f - 286.5f, 15);
+        [DllImport("user32.dll")]
+        internal static extern IntPtr SetFocus(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        internal static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+
+        static void Main(string[] args)
+        {
+            Leaguesharpicon = loadleaguesharp();
+            CustomEvents.Game.OnGameLoad += onGameLoad;
+            Game.OnWndProc += Game_OnWndProc;
+        }
+
+        private static void Game_OnWndProc(WndEventArgs args)
+        {
+            if ((args.Msg == (uint)WindowsMessages.WM_LBUTTONDOWN) && mouseonlsharp())
+            {
+                bringtofront();
+            } 
+        }
+
+        private static void onGameLoad(EventArgs args) {
+        Game.PrintChat("Loaded AssemblySelector by Seph");
+        }
+
+        private static IntPtr FindLeagueSharp()
+        {
+            return FindWindow(null, "LeagueSharp");
+        }
+
+
+        private static void bringtofront()
+        {
+          
+                Selector.ShowWindow(FindLeagueSharp(), 1);
+                Selector.SetForegroundWindow(FindLeagueSharp());
+                Selector.SetFocus(FindLeagueSharp());
+               
+            
+        }
+
+        private static Vector2 GetPosition(int width)
+        {
+            return new Vector2(Drawing.Width / 2f - width / 2f, 10);
+        }
+
+        private static Vector2 GetScaledVector(Vector2 vector)
+        {
+            return Vector2.Modulate(_scale, vector);
+        }
+
+        private static Render.Sprite loadleaguesharp()
+        {
+
+            _posLsharp = GetScaledVector(_posLsharp);
+
+            var loadlsharp= new Render.Sprite(Resources.lsharpicon, _posLsharp)
+            {
+                Scale = _scale,
+                Color = new ColorBGRA(255f, 255f, 255f, 20f)
+            };
+            loadlsharp.Position = GetPosition(loadlsharp.Width - 700);
+
+            loadlsharp.Show();
+            loadlsharp.Add(0);
+
+            return loadlsharp;
+        }
+
+        private static bool mouseonlsharp()
+        {
+            _posLsharp = GetScaledVector(_posLsharp);
+
+            var loadlsharp = new Render.Sprite(Resources.lsharpicon, _posLsharp)
+            {
+                Scale = _scale,
+                Color = new ColorBGRA(255f, 255f, 255f, 20f)
+            };
+
+            var pos = Utils.GetCursorPos();
+            var lsharpbutton = GetPosition(loadlsharp.Width - 700);
+
+            return ((pos.X >= lsharpbutton.X) && pos.X <= (lsharpbutton.X + loadlsharp.Width) && pos.Y >= lsharpbutton.Y && pos.Y <= (lsharpbutton.Y + loadlsharp.Height));
+        }
+
+
+
+
+    }
+}
