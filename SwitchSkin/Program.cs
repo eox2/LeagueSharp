ea50feed0a6e9d5ea96e330c22a831679bcd2255
SephLeague <SephLeague@users.noreply.github.com>
2015-05-02 22:07:36 -0400

    stringlists

--- SwitchSkin/Program.cs
+++ SwitchSkin/Program.cs
@@ -21,26 +21,52 @@ namespace SwitchSkin
             foreach (var hero in HeroManager.AllHeroes)
             {
                 var currenthero = hero;
-                var selectskin = menu.AddItem(new MenuItem("skin." + hero.Name, hero.ChampionName + " (" + hero.Name + ")").SetValue(new Slider(0, 0, 10)));
+                var newselect = menu.AddItem(new MenuItem("skin." + hero.Name, hero.ChampionName + " (" + hero.Name + ")").SetValue(new StringList(new[] { "Skin 0", "Skin 1", "Skin 2", "Skin 3", "Skin 4", "Skin 5", "Skin 6", "Skin 7", "Skin 8", "Skin 9", "Skin 10" }, 0)));
 
-                if (hero.Name == ObjectManager.Player.Name)
+                if (currenthero.Name == ObjectManager.Player.Name)
                 {
-                    currenthero.SetSkin(currenthero.ChampionName, selectskin.GetValue<Slider>().Value);
+                    currenthero.SetSkin(currenthero.ChampionName, GetSkinNumber(currenthero));
                 }
 
-                selectskin.ValueChanged += delegate
+                newselect.ValueChanged += delegate
                 {
-                    try
-                    {
-                        currenthero.SetSkin(currenthero.ChampionName, selectskin.GetValue<Slider>().Value);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.Write(e);
-                    }
+                   currenthero.SetSkin(currenthero.ChampionName, GetSkinNumber(currenthero));
                 };
             }
             menu.AddToMainMenu();
+        }
+
+        public static int GetSkinNumber(Obj_AI_Hero hero)
+        {
+            var strings = menu.Item("skin." + hero.Name).GetValue<StringList>();
+            switch (strings.SList[strings.SelectedIndex])
+            {
+                case "Skin 0":
+                    return 0;
+                case "Skin 1":
+                    return 1;
+                case "Skin 2":
+                    return 2;
+                case "Skin 3":
+                    return 3;
+                case "Skin 4":
+                    return 4;
+                case "Skin 5":
+                    return 5;
+                case "Skin 6":
+                    return 6;
+                case "Skin 7":
+                    return 7;
+                case "Skin 8":
+                    return 8;
+                case "Skin 9":
+                    return 9;
+                case "Skin 10":
+                    return 10;
+                default:
+                    Console.WriteLine(("def"));
+                    return 0;
+            }
         }
 
         static void FloatPropertyChange(GameObject sender, GameObjectFloatPropertyChangeEventArgs args)
@@ -53,7 +79,7 @@ namespace SwitchSkin
  
             if (args.OldValue.Equals(args.NewValue) && args.NewValue.Equals(hero.MaxHealth) && !hero.IsDead)
             {
-                hero.SetSkin(hero.ChampionName, menu.Item("skin." + hero.Name).GetValue<Slider>().Value);
+                hero.SetSkin(hero.ChampionName, GetSkinNumber(hero));
             }
         }
     }
