3fb3fdc7b4b5b48a8360eebda7514b529c212306
SephLeague <SephLeague@users.noreply.github.com>
2015-05-02 22:30:13 -0400

    x

--- SwitchSkin/Program.cs
+++ SwitchSkin/Program.cs
@@ -21,7 +21,7 @@ namespace SwitchSkin
             foreach (var hero in HeroManager.AllHeroes)
             {
                 var currenthero = hero;
-                var newselect = menu.AddItem(new MenuItem("skin." + hero.Name, hero.ChampionName + " (" + hero.Name + ")").SetValue(new StringList(new[] { "Skin 0", "Skin 1", "Skin 2", "Skin 3", "Skin 4", "Skin 5", "Skin 6", "Skin 7", "Skin 8", "Skin 9", "Skin 10" }, 0)));
+                var newselect = menu.AddItem(new MenuItem("skin." + hero.Name, hero.ChampionName + " (" + hero.Name + ")").SetValue(new StringList(new[] { "Skin 0", "Skin 1", "Skin 2", "Skin 3", "Skin 4", "Skin 5", "Skin 6", "Skin 7", "Skin 8", "Skin 9", "Skin 10" }, 1)));
 
                 if (currenthero.Name == ObjectManager.Player.Name)
                 {
@@ -30,6 +30,7 @@ namespace SwitchSkin
 
                 newselect.ValueChanged += delegate
                 {
+                   Console.WriteLine("setting to " + GetSkinNumber(currenthero));
                    currenthero.SetSkin(currenthero.ChampionName, GetSkinNumber(currenthero));
                 };
             }
@@ -39,34 +40,7 @@ namespace SwitchSkin
         public static int GetSkinNumber(Obj_AI_Hero hero)
         {
             var strings = menu.Item("skin." + hero.Name).GetValue<StringList>();
-            switch (strings.SList[strings.SelectedIndex])
-            {
-                case "Skin 0":
-                    return 0;
-                case "Skin 1":
-                    return 1;
-                case "Skin 2":
-                    return 2;
-                case "Skin 3":
-                    return 3;
-                case "Skin 4":
-                    return 4;
-                case "Skin 5":
-                    return 5;
-                case "Skin 6":
-                    return 6;
-                case "Skin 7":
-                    return 7;
-                case "Skin 8":
-                    return 8;
-                case "Skin 9":
-                    return 9;
-                case "Skin 10":
-                    return 10;
-                default:
-                    Console.WriteLine(("def"));
-                    return 0;
-            }
+            return strings.SelectedIndex;
         }
 
         static void FloatPropertyChange(GameObject sender, GameObjectFloatPropertyChangeEventArgs args)
