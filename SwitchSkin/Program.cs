797d2c6ca04c03cae9ce366279198affd0cfb24b
SephLeague <SephLeague@users.noreply.github.com>
2015-05-03 04:00:06 -0400

    improved

--- SwitchSkin/Program.cs
+++ SwitchSkin/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using LeagueSharp;
 using LeagueSharp.Common;
 
@@ -7,6 +8,8 @@ namespace SwitchSkin
     class Program
     {
         private static Menu menu;
+        private static Dictionary<String, int> ChampSkins = new Dictionary<String, int>();
+  
 
         static void Main(string[] args)
         {
@@ -14,34 +17,38 @@ namespace SwitchSkin
             Obj_AI_Hero.OnFloatPropertyChange += FloatPropertyChange;
         }
 
+
         static void GameLoad(EventArgs argss)
         {
              menu = new Menu("SwitchSkins", "Skinswitcher", true);
 
+             menu.AddItem(new MenuItem("forall", "Enable for all (lags menu)").SetValue(false));
+
             foreach (var hero in HeroManager.AllHeroes)
             {
+                if (!menu.Item("forall").GetValue<bool>() && hero.Name != ObjectManager.Player.Name)
+                {
+                    continue;
+                } 
+
                 var currenthero = hero;
-                var newselect = menu.AddItem(new MenuItem("skin." + hero.Name, hero.ChampionName + " (" + hero.Name + ")").SetValue(new StringList(new[] { "Skin 0", "Skin 1", "Skin 2", "Skin 3", "Skin 4", "Skin 5", "Skin 6", "Skin 7", "Skin 8", "Skin 9", "Skin 10" }, 1)));
 
-                if (currenthero.Name == ObjectManager.Player.Name)
-                {
-                    currenthero.SetSkin(currenthero.ChampionName, GetSkinNumber(currenthero));
-                }
+                var newselect = menu.AddItem(new MenuItem("skin." + hero.ChampionName, hero.ChampionName + " (" + hero.Name + ")").SetValue(new StringList(new[] { "Skin 0", "Skin 1", "Skin 2", "Skin 3", "Skin 4", "Skin 5", "Skin 6", "Skin 7", "Skin 8", "Skin 9", "Skin 10" }, 0)));
 
-                newselect.ValueChanged += delegate
+                ChampSkins.Add(hero.Name, newselect.GetValue<StringList>().SelectedIndex);
+
+                hero.SetSkin(hero.ChampionName, ChampSkins[hero.Name]);
+
+                newselect.ValueChanged += delegate(Object sender, OnValueChangeEventArgs args)
                 {
-                   Console.WriteLine("setting to " + GetSkinNumber(currenthero));
-                   currenthero.SetSkin(currenthero.ChampionName, GetSkinNumber(currenthero));
+                   ChampSkins[currenthero.Name] = args.GetNewValue<StringList>().SelectedIndex;
+                   currenthero.SetSkin(currenthero.ChampionName, ChampSkins[currenthero.Name]);
                 };
+
             }
             menu.AddToMainMenu();
         }
 
-        public static int GetSkinNumber(Obj_AI_Hero hero)
-        {
-            var strings = menu.Item("skin." + hero.Name).GetValue<StringList>();
-            return strings.SelectedIndex;
-        }
 
         static void FloatPropertyChange(GameObject sender, GameObjectFloatPropertyChangeEventArgs args)
         {
@@ -53,7 +60,7 @@ namespace SwitchSkin
  
             if (args.OldValue.Equals(args.NewValue) && args.NewValue.Equals(hero.MaxHealth) && !hero.IsDead)
             {
-                hero.SetSkin(hero.ChampionName, GetSkinNumber(hero));
+                hero.SetSkin(hero.ChampionName, ChampSkins[hero.Name]);
             }
         }
     }
