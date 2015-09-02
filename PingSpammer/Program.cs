044659110dd9b03839555329724a0be7fb567a59
Seph <SephLeague@users.noreply.github.com>
2015-09-02 19:06:45 -0400

    xD

---
+++ PingSpammer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace PingSpammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnLoad;
+        }
+
+        private static double lasttick;
+
+        static void OnLoad(EventArgs args)
+        {
+            if (Game.Time - lasttick >= 1000)
+            {
+                lasttick = Game.Time;
+                foreach (var hero in HeroManager.AllHeroes)
+                {
+                    Vector3 kappa = hero.Position;
+                    Game.SendPing(PingCategory.Normal, kappa);
+                }
+            }
+        }
+    }
+}
